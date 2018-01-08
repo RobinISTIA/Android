3ee63e3095e519729ce751ccf838707612659656
Robin <usrlocal@Gfixeetu101.ad.univ-angers.fr>
2018-01-08 15:02:46 +0100

    ceci est un texto pour me rappeler du contenu de ces modifs

---
+++ Sceance7TD/app/src/main/java/com/example/usrlocal/sceance7td/mainActivity.java
@@ -0,0 +1,14 @@
+package com.example.usrlocal.sceance7td;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class mainActivity extends AppCompatActivity {
+
+  
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+  }
+}
